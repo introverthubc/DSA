83e9afbebf7b312f2616b90aeb1eddd87b855ccc
Rudra Pratap <s4ty4m125@gmail.com>
2026-08-08 10:27:52 +0530

    Time: 1 ms (96.16%) | Memory: 43.5 MB (95.99%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -6,11 +6,7 @@ class Solution {
             }
         List<Boolean> result = new ArrayList<>();
         for (int i = 0; i < candies.length; i++) {
-            if (candies[i] + extraCandies >= max) {
-                result.add(true);
-            } else {
-                result.add(false);
-            }
+            result.add(candies[i]+extraCandies>=max);
         }return result;
     }
 }

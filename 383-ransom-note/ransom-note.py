089860899ec89b80768558b0eb2c077f5a863b0c
Rudra Pratap <s4ty4m125@gmail.com>
2026-08-18 12:14:23 +0530

    Time: 19 ms (63.30%) | Memory: 19.7 MB (8.67%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,7 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magazine_count = Counter(magazine)
+        magazine_count=Counter(magazine)
         for char,count in Counter(ransomNote).items():
-            if magazine_count[char] < count:
+            if magazine_count[char]<count:
                 return False
         return True
